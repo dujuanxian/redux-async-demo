855d99383c0fbba52a4e64a8c079857ac5474b48
杜娟 <jdu.xian@gmail.com>
2019-07-24 21:29:47 +0800

    get todos async

--- src/actions/getTodos.js
+++ src/actions/getTodos.js
@@ -1,21 +1,12 @@
 export const GET_TODOS = 'GET_TODOS';
 
 export default function getTodos() {
-  return {
-    type: GET_TODOS,
-    payload: [
-      {
-        "userId": 1,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-      },
-      {
-        "userId": 1,
-        "id": 2,
-        "title": "quis ut nam facilis et officia qui",
-        "completed": true
-      }
-    ]
-  }
+  return function getTodosThunk(dispatch) {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(data => dispatch({
+        type: GET_TODOS,
+        payload: data
+      }))
+  };
 }
