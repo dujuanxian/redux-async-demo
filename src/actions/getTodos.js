29fca99cf645a18e1b329bf2f086b28c0d2674f6
杜娟 <jdu.xian@gmail.com>
2020-05-26 11:52:23 +0800

    get todos

--- src/actions/getTodos.js
+++ src/actions/getTodos.js
@@ -1,12 +1,12 @@
 export const GET_TODOS = 'GET_TODOS';
 
 export default function getTodos() {
-  return function getTodosThunk(dispatch) {
+  return (dispatch) => (
     fetch('https://jsonplaceholder.typicode.com/todos')
       .then(response => response.json())
       .then(data => dispatch({
         type: GET_TODOS,
         payload: data
       }))
-  };
+  );
 }
